409ef40dbac93a77c4575d3f0958649825e468ef
Roman Tkachev <keitarofreeman@gmail.com>
2021-05-18 08:57:09 +0700

    6 lesson

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import { Route, Switch } from "react-router";
 import Header from "./Components/Header";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get("/db.json").then(({ data }) => {
+      setItems(data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -13,7 +23,7 @@ function App() {
             <Cart />
           </Route>
           <Route path="/">
-            <Home />
+            <Home items={items} />
           </Route>
         </Switch>
       </div>
